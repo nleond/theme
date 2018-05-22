bdde2d528ff2b961f0a705a7459fe10fb04aabc3
David Bromoiras <bromoiras.david@gmail.com>
2018-05-22 16:05:04 +0300

    first athena push

---
+++ assets/js/theme.js
@@ -0,0 +1,26 @@
+var MainModule = (function($){
+  
+  var extendLiveResults = function(){
+    
+  }
+  
+  var hideLiveResults = function(){
+    
+  }
+  
+  var menuHover = function(){
+    
+  }
+  
+  var switchAdvancedSearch(){
+    
+  }
+  
+  return{
+    extendLiveResults:extendLiveResults,
+    hideLiveResults:hideLiveResults,
+    menuHover:menuHover,
+    switchAdvancedSearch:switchAdvancedSearch
+  }
+  
+})(jQuery)
