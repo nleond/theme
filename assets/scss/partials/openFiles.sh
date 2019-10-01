5ee5e9b9a275f36585c550d8125dc6c03b791225
nleond <nikleonidakis@gmail.com>
2019-10-01 11:51:36 +0300

    added scss partials

--- assets/scss/partials/openFiles.sh
+++
@@ -1,6 +0,0 @@
-sudo gedit  _landing-data.scss &
-sudo gedit _header.scss &
-sudo gedit _landing-landing-section.scss &
-sudo gedit _landing-main.scss &
-sudo gedit _main.scss &
-sudo gedit _results-main.scss &
